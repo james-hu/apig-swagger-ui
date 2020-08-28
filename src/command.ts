a0903e52075f68a40b64afed60107d36988541d1
James Hu <james.hu.ustc@hotmail.com>
2020-08-29 00:20:48 +1000

    parse cli arguments

--- src/command.ts
+++ src/command.ts
@@ -1,29 +1,35 @@
+import micromatch = require('micromatch');
 import ApigSwaggerUi = require('.');
 import { APIGateway } from 'aws-sdk';
 
-export const execute: (options: ReturnType<ApigSwaggerUi["parse"]>) => Promise<void> = async options => {
-    const apig = new APIGateway();
+export const execute: (options: typeof ApigSwaggerUi.Options) => Promise<any> = async options => {
+    const apig = new APIGateway({region: options.flags.region});
 
     const domainNames = (await apig.getDomainNames({limit: 500}).promise())?.items;
     if (domainNames != null) {
         for (let domainName of domainNames) {
-            console.log("##################")
-            console.log(domainName);
+            const supportHttps = domainName.securityPolicy != null;
             const mappings = (await apig.getBasePathMappings({domainName: domainName.domainName!, limit: 500}).promise())?.items;
             if (mappings != null) {
                 for (let mapping of mappings) {
-                    console.log(mapping);
-                    const exported = await apig.getExport({
-                        restApiId: mapping.restApiId!,
-                        stageName: mapping.stage!,
-                        exportType: 'oas30',
-                        parameters: {
-
-                        }
-                    }).promise();
-                    const spec = exported.body?.toString('utf8');
-                    console.log(spec);
-                    
+                    const basePath = mapping.basePath === '(none)' ? '' : mapping.basePath;
+                    const domainAndBasePath = `${domainName.domainName}/${basePath}`;
+                    const shouldInclude = micromatch.isMatch(domainAndBasePath, options.flags.include);
+                    const shouldExclude = options.flags.exclude == null ? false : micromatch.isMatch(domainAndBasePath, options.flags.exclude);
+                    if (shouldInclude && !shouldExclude) {
+                        const baseUrl = `${supportHttps ? 'https' : 'http'}://${domainAndBasePath}`;
+                        console.log(baseUrl);
+                        const exported = await apig.getExport({
+                            restApiId: mapping.restApiId!,
+                            stageName: mapping.stage!,
+                            exportType: 'oas30',
+                            parameters: {
+    
+                            }
+                        }).promise();
+                        const spec = exported.body?.toString('utf8');
+                        //console.log(spec);
+                    }
                 }
             }
         }
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,44 @@
 import {Command, flags} from '@oclif/command'
+import * as Parser from '@oclif/parser';
 import {execute} from './command';
 
+
 class ApigSwaggerUi extends Command {
-  static description = 'describe the command here'
+  static Options: ApigSwaggerUiOptions  // just to hold the type
+  static description = 'Command line tool for generating OpenAPI spec and SwaggerUI from AWS API Gateway'
 
   static flags = {
     // add --version flag to show CLI version
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
+    region: flags.string({char: 'r', description: 'AWS region'}),
+    include: flags.string({char: 'i', default: '*/*', multiple: true, description: 'custom domains to include'}),
+    exclude: flags.string({char: 'x', multiple: true, description: 'custom domains to exclude'}),
     // flag with no value (-f, --force)
     force: flags.boolean({char: 'f'}),
   }
 
-  static args = [{name: 'file'}]
+  static args = [
+    {name: 'path' as const, description: 'path for putting generated files'}
+  ]
 
+  
   async run() {
-    const options = this.parse(ApigSwaggerUi)
-    const {args, flags} = options
-
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
-
+    const options = this.parse<GetF<typeof ApigSwaggerUi>, ArgType>(ApigSwaggerUi)
+    console.log(options);
     return execute(options);
-
   }
 
 
 }
 
-export = ApigSwaggerUi
+export = ApigSwaggerUi;
+
+type GetF<T> = T extends Parser.Input<infer F> ? F : never
+type GetArgNames<T> = T extends {name: infer A}[] ? A : never
+type ArgNames = GetArgNames<typeof ApigSwaggerUi.args>
+type ArgType = {
+  [x in ArgNames]: string;
+};
+type ApigSwaggerUiOptions = Parser.Output<GetF<typeof ApigSwaggerUi>, ArgType>
